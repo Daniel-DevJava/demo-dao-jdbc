208b067c35727857c8ff1e10545db874cf6ae67a
Daniel Dias <danieldias.devjava@gmail.com>
2023-09-27 08:38:27 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -36,5 +37,9 @@ public class Program {
 			System.out.println(obj);
 		}
 
+		System.out.println("\n--- TEST four: seller insert ----");
+		Seller newSeller = new Seller(null, "Alex", "alex@gmail.com", new Date(), 5000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("New id Inserted! = " + newSeller.getId());
 	}
 }
